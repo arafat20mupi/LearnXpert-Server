bbc52a8f475d97682a824aa33e43bc754f186ba6
unknown <arafat.innotimes.net@gmail.com>
2024-12-27 16:28:43 +0600

    Setup Done

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./Config/dbConfig");
+require("dotenv").config();
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+connectDB();
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+//  Home route
+app.get("/", (req, res) => {
+  res.send("hello Developer");
+});
+
+
+// Server listening
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
